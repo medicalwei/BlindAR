1e10f8e01d10e1b2986ebcea46070de7816c24fb
Ming-Ting Yao Wei <medicalwei@gmail.com>
2011-05-09 20:19:44 +0800

    hand gesture detection and display critical points

--- src/edu/ntou/blindar/DrawOnTop.java
+++ src/edu/ntou/blindar/DrawOnTop.java
@@ -1,5 +1,7 @@
 package edu.ntou.blindar;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -14,6 +16,9 @@ class DrawOnTop extends View {
 	int mThreshold=20, mDotted;
 	boolean mDrawEdges=true;
 	boolean areaDetected=false;
+	
+	byte lastDetectionMap[];
+	short skinColor[] = {142, 120};
 
     public DrawOnTop(Context context) {
         super(context);
@@ -26,126 +31,208 @@ class DrawOnTop extends View {
         {
         	Paint paint=new Paint();
         	paint.setARGB(255, 127, 127, 127);
-        	// canvas.drawColor(Color.argb(127,0,0,0));
-        	canvas.drawBitmap(edgeDetect(mYUVData, mImageWidth, mImageHeight), 0, 0, null);
-        	canvas.drawText("T: " + mThreshold + ", D: " + mDotted, 10, 10, paint);
+        	
+        	int hand[] = handDetect(mYUVData, mImageWidth, mImageHeight);
+        	
+        	Bitmap result = Bitmap.createBitmap(
+        			mImageWidth/2, mImageHeight/2, Bitmap.Config.RGB_565);
+        	
+        	result.setPixels(
+        			hand,0,mImageWidth/2,0,0,mImageWidth/2,mImageHeight/2);
+        	
+        	canvas.drawBitmap(result, 0, 0, null);
         } // end if statement
         
         super.onDraw(canvas);
-        
     }
-    
-    /* Use Y only, no Cb nor Cr at all. */
-    
-	public Bitmap edgeDetect(byte fg[], int width, int height)
-	{
-		Bitmap result = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-		if(!mDrawEdges) return result;
-		mDotted = 0;
-		int[] resultBuffer = new int[height*width];
-		short[] N = new short[height*width];
-		// int[] bitmapRGB = new int[height*width];
+   
+	
+	class PeakData {
+		int position;
+		int value;
 		
-		decodeYUV(resultBuffer, fg, width, height);
-		
-		for (int y = 1; y < height-1; y++) {
-			
-			for (int x = 1; x < width-1; x++) {
-				
-				int offset = y*width + x;
-
-				/*  Unsigned byte
-				 *  Reference:
-				 *  
-				 *  public static int unsignedByteToInt(byte b) {
-				 *  	return (int) b & 0xFF;
-    			 *  }
-				 */
-				
-				short O = (short) (fg[offset] & 0xFF);
-				
-				/*
-				 * 
-				 * 
-				short A = (short) ((fg[offset-1] & 0xFF) - O); // x-1, y
-				short B = (short) ((fg[offset-width] & 0xFF) - O); // x, y-1
-				
-				N[offset] = (short) (A+B);
-				*/
-				
-				N[offset] = (short) ((fg[offset-1] & 0xFF) - O);
-
-			}
-		}
-
-		for (int y = 2; y < height-2; y++) {
-			for (int x = 2; x < width-2; x++) {
-				int offset = y*width + x;
-				if (Math.abs(N[offset])>mThreshold)
-				{
-					if (N[offset]>0)
-					{
-						resultBuffer[offset] = Color.RED;	
-					}
-					else if (N[offset]<0)
-					{
-						resultBuffer[offset] = Color.BLUE;
-					}
-					mDotted += 1;
-				}
-				else
-				{
-					if(areaDetected)
-					{
-						resultBuffer[offset] = Color.GREEN;
-					}
-				}
-			}
-		}
-		
-		/* Adjust threshold by looking at the number of dots */
-		mThreshold += (mDotted-5000) / 500;
-		
-		if(mThreshold<5) mThreshold = 5;
-		else if(mThreshold>255) mThreshold = 255;
-		
-		result.setPixels(resultBuffer,0,width,0,0,width,height);
-		
-		return result;
-	}
-
-	public static void decodeYUV(int[] out, byte[] fg, int width, int height) {
-		final int sz = width * height;
-		int i, j;
-		int Y, Cr = 0, Cb = 0;
-		for(j = 0; j < height; j++) {
-			int pixPtr = j * width;
-			final int jDiv2 = j >> 1;
-			for(i = 0; i < width; i++) {
-				Y = fg[pixPtr]; if(Y < 0) Y += 255;
-				if((i & 0x1) != 1) {
-					final int cOff = sz + jDiv2 * width + (i >> 1) * 2;
-					Cb = fg[cOff];
-					if(Cb < 0) Cb += 127; else Cb -= 128;
-					Cr = fg[cOff + 1];
-					if(Cr < 0) Cr += 127; else Cr -= 128;
-				}
-				int R = Y + Cr + (Cr >> 2) + (Cr >> 3) + (Cr >> 5);
-				if(R < 0) R = 0; else if(R > 255) R = 255;
-				int G = Y - (Cb >> 2) + (Cb >> 4) + (Cb >> 5) - (Cr >> 1) + (Cr >> 3) + (Cr >> 4) + (Cr >> 5);
-				if(G < 0) G = 0; else if(G > 255) G = 255;
-				int B = Y + Cb + (Cb >> 1) + (Cb >> 2) + (Cb >> 6);
-				if(B < 0) B = 0; else if(B > 255) B = 255;
-				out[pixPtr++] = 0xff000000 + (B << 16) + (G << 8) + R;
-			}
+		PeakData(int i_position, int i_value)
+		{
+			position = i_position;
+			value = i_value;
 		}
 	}
 	
-	public static int colorOffset(int a, int b) {
-		int R = Math.abs((a & 0x000000ff) - (b & 0x000000ff));
-		int G = Math.abs(((a & 0x0000ff00) >> 8) - (b & (0x0000ff00) >> 8));
-		int B = Math.abs(((a & 0x00ff0000) >> 16) - ((b & 0x00ff0000) >> 16));
-		return R+G+B;
+    /* Use Y only, no Cb nor Cr at all. */
+    
+	public int[] handDetect(byte fg[], int width, int height)
+	{
+		width = width / 2;
+		height = height / 2;
+		
+		int pixels = height * width;
+		short CbCr[][] = new short[pixels][2];
+		int bitmap[] = new int[pixels];
+		byte detectionMap[] = new byte[pixels];
+		
+		if (lastDetectionMap == null)
+		{
+			lastDetectionMap = new byte[pixels];
+		}
+		
+		decodeYUVtoCbCr(CbCr, fg, pixels);
+		
+		int centerPos = (width + pixels) / 2;
+		
+		short spotColor[] = CbCr[centerPos];
+		
+		
+		if (CbCrOffset(spotColor, skinColor) > 100)
+		{
+			lastDetectionMap = detectionMap;
+			return bitmap; /* blank */
+		}
+		
+		skinColor[0] = spotColor[0];
+		skinColor[1] = spotColor[1];
+		
+		
+		int colors[]=
+		{Color.rgb(0,0,255),
+		Color.rgb(0,0,127),
+		Color.rgb(0,0,63),
+		Color.rgb(0,0,0),
+		Color.rgb(63,0,0),
+		Color.rgb(127,0,0),
+		Color.rgb(255,0,0)};
+
+		int widthTotal[] = new int[width];
+		int heightTotal[] = new int[height];
+		
+		for(int pos=0;pos<pixels;pos++)
+		{
+
+			int offset = CbCrOffset(CbCr[pos], spotColor);
+			
+			if (pos < width || pos >= pixels-width)
+			{
+				continue;
+			}
+			
+			if (offset < 100)
+			{
+				if (offset < 64)
+				{
+					if (offset < 36)
+					{
+						detectionMap[pos] = 3;
+					}
+					else
+					{
+						detectionMap[pos] = 2;
+					}
+				}
+				else
+				{
+					detectionMap[pos] = 1;
+				}
+			}
+			else
+			{
+				detectionMap[pos] = 0;
+				continue;
+			}
+		}
+		
+		for(int pos=0;pos<pixels;pos++)
+		{
+			int d = detectionMap[pos]-lastDetectionMap[pos];
+			bitmap[pos] = colors[d+3];
+			widthTotal[pos%width] += d;
+			heightTotal[pos/width] += d;
+		}
+		
+		ArrayList <PeakData> widthPeaks = new ArrayList<PeakData>();
+		ArrayList <PeakData> heightPeaks = new ArrayList<PeakData>();
+		boolean isIncreasing = false;
+		
+		for(int i=1;i<width;i++)
+		{
+			int diff = widthTotal[i] - widthTotal[i-1];
+			/*
+			if (widthTotal[i] > 0)
+			{
+				bitmap[i] = Color.rgb(0,(widthTotal[i] << 1),0);
+			}
+			else
+			{
+				bitmap[i] = Color.rgb(0,0,-(widthTotal[i] << 1));
+			}
+			*/
+			
+			if (diff > 0 && !isIncreasing)
+			{
+				isIncreasing = true;
+			}
+			else if (diff < 0 && isIncreasing)
+			{
+				isIncreasing = false;
+				if(widthTotal[i]>80)
+				{
+					widthPeaks.add(new PeakData(i, widthTotal[i]));
+					bitmap[i] = Color.rgb(0,255,0);
+				}
+			}
+			
+		}
+		
+		for(int i=1;i<height;i++)
+		{
+			int diff = heightTotal[i] - heightTotal[i-1];
+			/*
+			if (heightTotal[i] > 0)
+			{
+				bitmap[i*width] = Color.rgb(0,(heightTotal[i] << 1),0);
+			}
+			else
+			{
+				bitmap[i*width] = Color.rgb(0,0,-(heightTotal[i] << 1));
+			}
+			*/
+			
+			if (diff > 0 && !isIncreasing)
+			{
+				isIncreasing = true;
+			}
+			else if (diff < 0 && isIncreasing)
+			{
+				isIncreasing = false;
+				if(heightTotal[i]>80)
+				{
+					heightPeaks.add(new PeakData(i, heightTotal[i]));
+					bitmap[i*width] = Color.rgb(0,255,0);
+				}
+			}
+		}
+		
+		
+		
+		lastDetectionMap = detectionMap;
+		
+		return bitmap;
 	}
+
+	public static void decodeYUVtoCbCr(short[][] out, byte[] fg, int sz) {
+		final int uvStart = sz << 2;
+		int uvPoint = uvStart;
+		
+		for(int i = 0; i < sz; i++) {
+			out[i][0] = (short) (fg[uvPoint] & 0xFF);
+			out[i][1] = (short) (fg[uvPoint+1] & 0xFF);
+			uvPoint += 2;
+		}
+	}
+	
+	public static int CbCrOffset(short a[], short b[]) {
+		int cb = a[0]-b[0];
+		int cr = a[1]-b[1];
+		return cb*cb + cr*cr;
+	}
+
 }
 
